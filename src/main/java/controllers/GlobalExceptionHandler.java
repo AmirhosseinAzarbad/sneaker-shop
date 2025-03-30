9347269f6dd2a0795b3bbe7b8762016da180b120
speedy <thenewamirech@gmail.com>
2025-03-30 16:04:02 +0330

    Added GlobalExceptionHandler

---
+++ src/main/java/controllers/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package controllers;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<String> handleNotFound(EmptyResultDataAccessException e) {
+        return new ResponseEntity<>("Entity not found", HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        return new ResponseEntity<>("Internal server Error", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
