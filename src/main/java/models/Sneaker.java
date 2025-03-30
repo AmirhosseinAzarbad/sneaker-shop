c611d33ca5defa8958b8e3bd5e6be5b653d15b83
speedy <thenewamirech@gmail.com>
2025-03-30 09:53:27 +0330

    Added Model(Sneaker)

---
+++ src/main/java/models/Sneaker.java
@@ -0,0 +1,22 @@
+package models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Sneaker {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String brand;
+    private Double price;
+    private String size;
+    private String color;
+
+}
