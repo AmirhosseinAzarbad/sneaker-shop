5c6adf92c18bb59a5e4d0e939c376e7084e4bc2f
speedy <thenewamirech@gmail.com>
2025-03-30 09:55:39 +0330

    Added Repository(SneakerRepository)

---
+++ src/main/java/repositories/SneakerRepository.java
@@ -0,0 +1,9 @@
+package repositories;
+
+import models.Sneaker;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SneakerRepository extends JpaRepository<Sneaker,Long> {
+}
