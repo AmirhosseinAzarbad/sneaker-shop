aa92dffddab5cbc46bd0f930a4012b3f58f1550d
speedy <thenewamirech@gmail.com>
2025-04-25 01:51:28 +0330

    Refactored SneakerService and removed SneakerCrudTes

--- src/main/java/ir/jiring/sneakershop/services/SneakerService.java
+++ src/main/java/ir/jiring/sneakershop/services/SneakerService.java
@@ -1,52 +1,80 @@
 package ir.jiring.sneakershop.services;
 
 
-import jakarta.persistence.EntityNotFoundException;
+import ir.jiring.sneakershop.dto.sneaker.SneakerAddRequest;
+import ir.jiring.sneakershop.dto.sneaker.SneakerResponse;
+import ir.jiring.sneakershop.dto.sneaker.SneakerUpdateRequest;
+import ir.jiring.sneakershop.mapper.SneakerMapper;
 import ir.jiring.sneakershop.models.Sneaker;
-import org.springframework.beans.BeanUtils;
-import org.springframework.stereotype.Service;
+import ir.jiring.sneakershop.models.SneakerVariant;
 import ir.jiring.sneakershop.repositories.SneakerRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class SneakerService {
 
-    // Constructor injection
     private final SneakerRepository sneakerRepository;
 
     public SneakerService(SneakerRepository sneakerRepository) {
         this.sneakerRepository = sneakerRepository;
     }
 
-
     // Create Sneaker
-    public Sneaker addSneaker(Sneaker sneaker) {
-        return sneakerRepository.save(sneaker);
+    @Transactional
+    public SneakerResponse addSneaker(SneakerAddRequest request) {
+        Sneaker sneaker = new Sneaker();
+        sneaker.setName(request.getName());
+        sneaker.setBrand(request.getBrand());
+        sneaker.setPrice(request.getPrice());
+        sneakerRepository.save(sneaker);
+        return SneakerMapper.toSneakerResponseDTO(sneaker);
     }
 
-    // Read Sneaker
-    public Sneaker getSneaker(Long id) {
-        return sneakerRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Sneaker not found"));
-    }
 
-    public Iterable<Sneaker> getAllSneakers() {
-        return sneakerRepository.findAll();
+    public Iterable<SneakerResponse> getAllSneakers() {
+        List<Sneaker> sneakers = sneakerRepository.findAll();
+
+        return sneakers.stream()
+                .map(SneakerMapper::toSneakerResponseDTO)
+                .collect(Collectors.toList());
     }
 
     // Update Sneaker
-    public Sneaker updateSneaker(Long id, Sneaker sneaker) {
-        Sneaker existingSneaker = sneakerRepository.findById(id)
+    @Transactional
+    public SneakerResponse updateSneaker(UUID id, SneakerUpdateRequest request) {
+        Sneaker sneaker = sneakerRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Sneaker not found"));
+        if (request.getBrand() != null) {
+            sneaker.setBrand(request.getBrand());
+        }
+        if (request.getName() != null) {
+            sneaker.setName(request.getName());
+        }
+        if (request.getPrice() != null) {
+            BigDecimal newPrice = request.getPrice();
+            sneaker.setPrice(newPrice);
 
-        BeanUtils.copyProperties(sneaker, existingSneaker, "id");
-//            sneakerRepository.save(existingSneaker);     will be done automatically by @Transactional
-        return existingSneaker;
-
+            for (SneakerVariant variant : sneaker.getVariants()) {
+                variant.setPrice(newPrice);
+            }
+        }
+        sneakerRepository.save(sneaker);
+        return SneakerMapper.toSneakerResponseDTO(sneaker);
     }
 
     // Delete Sneaker
-    public void deleteSneaker(Long id) {
-        sneakerRepository.deleteById(id);
+    @Transactional
+    public void deleteSneaker(UUID id) {
+        Sneaker sneaker = sneakerRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Sneaker not found"));
+        sneakerRepository.delete(sneaker);
     }
 
 }
