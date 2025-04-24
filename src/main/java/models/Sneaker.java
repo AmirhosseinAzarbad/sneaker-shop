38eb2e924fc9e155ad30cb416b6dc9a0b1ca782d
speedy <thenewamirech@gmail.com>
2025-04-25 00:02:01 +0330

    Sneaker updated

--- src/main/java/models/Sneaker.java
+++ src/main/java/models/Sneaker.java
@@ -1,22 +1,31 @@
 package models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Data
 @Entity
-public class Sneaker {
+public class  Sneaker {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    private UUID id;
+
+    @Column(name = "name1",nullable = false)
     private String name;
+
+    @Column(name = "brand1",nullable = false)
     private String brand;
-    private Double price;
-    private String size;
-    private String color;
+
+    @Column(name = "price1",nullable = false)
+    private BigDecimal price;
+
+    @OneToMany(mappedBy = "sneaker", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    private List<SneakerVariant> variants = new ArrayList<>();
 
 }
