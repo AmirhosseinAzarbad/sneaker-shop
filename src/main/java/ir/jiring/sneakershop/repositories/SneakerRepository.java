44a2a4f125e447f918b3e333fd91d440e2a81166
speedy <thenewamirech@gmail.com>
2025-04-25 01:21:15 +0330

    Updated SneakerRepository(UUID instead of Long)

--- src/main/java/ir/jiring/sneakershop/repositories/SneakerRepository.java
+++ src/main/java/ir/jiring/sneakershop/repositories/SneakerRepository.java
@@ -4,6 +4,8 @@ import ir.jiring.sneakershop.models.Sneaker;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface SneakerRepository extends JpaRepository<Sneaker,Long> {
+public interface SneakerRepository extends JpaRepository<Sneaker, UUID> {
 }
