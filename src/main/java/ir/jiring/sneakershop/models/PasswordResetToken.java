c76b9a329b2ff70f688bc14a883592e7edb5c5a0
speedy <thenewamirech@gmail.com>
2025-04-25 21:53:55 +0330

    Added PasswordResetToken

---
+++ src/main/java/ir/jiring/sneakershop/models/PasswordResetToken.java
@@ -0,0 +1,29 @@
+package ir.jiring.sneakershop.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class  PasswordResetToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @OneToOne
+    private User user;
+
+    private LocalDateTime expiryDate;
+
+
+}
