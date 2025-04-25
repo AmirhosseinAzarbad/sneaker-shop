4898680bdc8f277045a3a61361d524e488add821
speedy <thenewamirech@gmail.com>
2025-04-25 17:19:52 +0330

    Updated UserRepository(added findByPhoneNumber and added findByRole)

--- src/main/java/ir/jiring/sneakershop/repositories/UserRepository.java
+++ src/main/java/ir/jiring/sneakershop/repositories/UserRepository.java
@@ -5,12 +5,17 @@ import ir.jiring.sneakershop.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
     Optional<User> findByUsername(String username);
 
     boolean existsByRole(Role role);
 
+    Optional<User> findByPhoneNumber(String phoneNumber);
+
+    List<User> findByRole(Role role);
 }
