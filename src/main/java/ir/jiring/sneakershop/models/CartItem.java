e39c4405246bb4cf91d1842f0225061a52e3c0d1
speedy <thenewamirech@gmail.com>
2025-04-26 22:41:37 +0330

    Added CartItem(entity)

---
+++ src/main/java/ir/jiring/sneakershop/models/CartItem.java
@@ -0,0 +1,38 @@
+package ir.jiring.sneakershop.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class CartItem implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private UUID id = UUID.randomUUID();
+
+    @ManyToOne
+    private Cart cart;
+
+    @ManyToOne
+    private SneakerVariant variant;
+
+    private int quantity;
+
+    private BigDecimal priceAtTime;
+
+    @Column(nullable = false)
+    private boolean priceConfirmed = true;
+
+}
