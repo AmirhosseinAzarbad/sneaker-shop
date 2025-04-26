f5f680de92fa0b1b36a25c9ef981edccf5697df6
speedy <thenewamirech@gmail.com>
2025-04-27 00:10:48 +0330

    Added RedisCart(entity)

---
+++ src/main/java/ir/jiring/sneakershop/models/RedisCart.java
@@ -0,0 +1,29 @@
+package ir.jiring.sneakershop.models;
+
+import ir.jiring.sneakershop.enums.CartStatus;
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import jakarta.validation.constraints.NotNull;
+
+@Data
+public class RedisCart implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @NotNull
+    private UUID id = UUID.randomUUID();
+
+    @NotNull
+    private Long userId;
+
+    @NotNull
+    private CartStatus status = CartStatus.PENDING;
+
+    private List<RedisCartItem> items = new ArrayList<>();
+
+}
