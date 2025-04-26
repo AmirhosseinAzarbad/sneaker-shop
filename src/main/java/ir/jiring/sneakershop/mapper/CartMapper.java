467bce51b72deb9d4c22d8f96dcd07b02755cf4a
speedy <thenewamirech@gmail.com>
2025-04-27 00:58:02 +0330

    Updated CartMapper(Cart <--> RedisCart)

--- src/main/java/ir/jiring/sneakershop/mapper/CartMapper.java
+++ src/main/java/ir/jiring/sneakershop/mapper/CartMapper.java
@@ -2,10 +2,13 @@ package ir.jiring.sneakershop.mapper;
 
 import ir.jiring.sneakershop.dto.cart.CartItemResponse;
 import ir.jiring.sneakershop.dto.cart.CartResponse;
-import ir.jiring.sneakershop.models.Cart;
-import ir.jiring.sneakershop.models.CartItem;
+import ir.jiring.sneakershop.models.*;
+import ir.jiring.sneakershop.repositories.SneakerVariantRepository;
+import ir.jiring.sneakershop.repositories.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 
 import java.math.BigDecimal;
+import java.util.stream.Collectors;
 
 public class CartMapper {
 
@@ -28,5 +31,48 @@ public class CartMapper {
         res.setTotalAmount(cart.getTotalAmount());
         res.setStatus(cart.getStatus().name());
         return res;
+    }
+
+    public static RedisCart toRedisCart(Cart cart) {
+        RedisCart rc = new RedisCart();
+        rc.setId(cart.getId());
+        rc.setUserId(cart.getUser().getUserId());
+        rc.setStatus(cart.getStatus());
+        rc.setItems(cart.getItems().stream().map(item -> {
+            RedisCartItem ri = new RedisCartItem();
+            ri.setId(item.getId());
+            ri.setVariantId(item.getVariant().getId());
+            ri.setQuantity(item.getQuantity());
+            ri.setPriceAtTime(item.getPriceAtTime());
+            ri.setPriceConfirmed(item.isPriceConfirmed());
+            return ri;
+        }).collect(Collectors.toList()));
+        return rc;
+    }
+
+    public static Cart fromRedisCart(RedisCart rc,
+                                     UserRepository userRepo,
+                                     SneakerVariantRepository variantRepo) {
+        Cart cart = new Cart();
+        cart.setId(rc.getId());
+        cart.setStatus(rc.getStatus());
+
+        userRepo.findById(rc.getUserId())
+                .ifPresent(cart::setUser);
+
+        cart.setItems(rc.getItems().stream().map(ri -> {
+            CartItem item = new CartItem();
+            item.setId(ri.getId());
+            SneakerVariant variant = variantRepo.findById(ri.getVariantId())
+                    .orElseThrow(() -> new EntityNotFoundException("Variant not found for id: " + ri.getVariantId()));
+            item.setVariant(variant);
+            item.setQuantity(ri.getQuantity());
+            item.setPriceAtTime(ri.getPriceAtTime());
+            item.setPriceConfirmed(ri.isPriceConfirmed());
+            item.setCart(cart);
+            return item;
+        }).collect(Collectors.toList()));
+
+        return cart;
     }
 }
