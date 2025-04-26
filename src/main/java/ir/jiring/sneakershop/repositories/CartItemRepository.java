13db95434cbafa20c6c844ad5e032c69c0d95aef
speedy <thenewamirech@gmail.com>
2025-04-26 23:45:49 +0330

    Added CartItemRepository

---
+++ src/main/java/ir/jiring/sneakershop/repositories/CartItemRepository.java
@@ -0,0 +1,7 @@
+package ir.jiring.sneakershop.repositories;
+
+import ir.jiring.sneakershop.models.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+}
