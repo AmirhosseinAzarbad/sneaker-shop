68362fd9c4a42a2333bb15ac867c924ccb923c40
speedy <thenewamirech@gmail.com>
2025-07-19 22:42:02 +0330

    JwtAuthenticationFilter(Simplified getting authorities of a User)

--- src/main/java/ir/jiring/sneakershop/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/ir/jiring/sneakershop/security/jwt/JwtAuthenticationFilter.java
@@ -8,14 +8,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Collections;
+import java.util.Collection;
 import java.util.List;
 
 @Component
@@ -62,9 +62,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 User user = userRepository.findByUsername(username)
                         .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
-                List<SimpleGrantedAuthority> authorities = Collections.singletonList(
-                        new SimpleGrantedAuthority("ROLE_" + user.getRole().name())
-                );
+                Collection<? extends GrantedAuthority> authorities = user.getAuthorities();
                 UsernamePasswordAuthenticationToken authentication =
                         new UsernamePasswordAuthenticationToken(username, null, authorities);
                 SecurityContextHolder.getContext().setAuthentication(authentication);
