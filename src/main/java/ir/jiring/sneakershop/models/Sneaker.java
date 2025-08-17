4165ed704cceb72f86611b78d6b34926ad4f974f
speedy <thenewamirech@gmail.com>
2025-08-17 21:05:22 +0330

    Added Sneaker as an index in Elasticsearch

--- src/main/java/ir/jiring/sneakershop/models/Sneaker.java
+++ src/main/java/ir/jiring/sneakershop/models/Sneaker.java
@@ -1,20 +1,27 @@
 package ir.jiring.sneakershop.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Document(indexName = "sneakers")
 @Data
 @Entity
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class  Sneaker {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private UUID id;
+    @Field(type = FieldType.Keyword)
+    @Column(nullable = false, updatable = false)
+    private UUID id = UUID.randomUUID();
 
     @Column(name = "name1",nullable = false)
     private String name;
